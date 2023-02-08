7388caf103d313998d3f7411a017e973e9f57203
Megan Smith <megans766@gmail.com>
2023-02-08 17:14:41 -0500

    create isComplete in profile model

--- models/profile.js
+++ models/profile.js
@@ -6,7 +6,11 @@ const Schema = mongoose.Schema
 const profileSchema = new Schema({
   name: String,
   photo: String,
-  goals: [{ type: Schema.Types.ObjectId, ref: 'GoalList' }]
+  goals: [{ type: Schema.Types.ObjectId, ref: 'GoalList' }],
+  isComplete: { 
+    type: Boolean,
+    default: false
+  }
 }, {
   timestamps: true,
 })
